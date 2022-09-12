e63a1b41fa39a179a4ab5d619788e7f9ff3166e0
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-12 17:07:37 +0200

    created Products.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
+  
+//   includiamo il file dei prodotti
+  include_once __DIR__ . 'models/Products.php';
 
 
 
---
+++ models/Products.php
@@ -0,0 +1,25 @@
+<?php
+
+class Products
+{
+    public $name;
+    public $image;
+    public $description;
+    public $price;
+    public $discount = 0;
+
+    //  costruttore
+    public function  __construct($name, $image, $description, $price, $discount)
+    {
+      
+    }
+
+
+    
+
+}
+
+
+
+
+?>
