04e59d2fea930480165e7676a71e518188a7e656
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-12 17:32:36 +0200

    Products.php methods added

--- models/Products.php
+++ models/Products.php
@@ -8,14 +8,64 @@ class Products
     public $price;
     public $discount = 0;
 
-    //  costruttore
+    //  Costruttore
     public function  __construct($name, $image, $description, $price, $discount)
     {
-      
+        $this->setName($name);
+        $this->setImage($image);
+        $this->setDescription($description);
+        $this->setPrice($price);
+        $this->setDiscount($discount);
     }
 
+    // Metodi GETTER
+    public function getName(){
+        return $this->name;
+    }
 
+    public function getImage(){
+        return $this->image;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function getDiscount(){
+        return $this->discount;
+    }
+
+    // Metodi SETTER
+    public function setName($name)
+    {
+        if(strlen($name) <= 0) return false;
+        $this->name = $name;
+        return true;
+    }
+
+    public function setImage($image)
+    {
+        if(!$image) return 'Immagine non disponibile';
+        $this->image = $image;
+        return true;
+    }
+
+    public function setDescription($description)
+    {
+        if(!$description) return 'Descrizione non disponibile';
+        $this->description = $description;
+        return true;
+    }
     
+    public function setPrice($price)
+    {
+        if(!is_numeric($price) || $price <= 0) return;
+        $this->price = $price;
+    }
 
 }
 
