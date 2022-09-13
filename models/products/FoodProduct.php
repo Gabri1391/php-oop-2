5a11220b633c113ccba7c12f811bf8eb265408b1
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-13 16:55:07 +0200

    FoodProduct.php fixed

--- models/products/FoodProduct.php
+++ models/products/FoodProduct.php
@@ -4,20 +4,75 @@
 
    class FoodProduct extends Product 
    {
-      protected $id;
-      protected $flavour;
-      protected $production_country;
+      private $flavour;
+      private $production_country;
+      private $expiration;  
 
-      public function  __construct($name, $image, $description, $price, $discount, 
-      $id, $flavour, $production_country)
+
+      public function  __construct($name, $description, $price, 
+       $flavour, $production_country, $expiration)
       {
-         parent:: __construct($name, $image, $description, $price, $discount);
-         $this->setId($id);
+         parent:: __construct($name, $description, $price);
          $this->setFlavour($flavour);
          $this->setProduction_country($production_country);
+         $this->setExpiration($expiration);
       }
 
 
+
+      /**
+       * Get the value of flavour
+       */
+      public function getFlavour()
+      {
+            return $this->flavour;
+      }
+
+      /**
+       * Set the value of flavour
+       */
+      public function setFlavour($flavour): self
+      {
+            $this->flavour = $flavour;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of production_country
+       */
+      public function getProductionCountry()
+      {
+            return $this->production_country;
+      }
+
+      /**
+       * Set the value of production_country
+       */
+      public function setProductionCountry($production_country): self
+      {
+            $this->production_country = $production_country;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of expiration
+       */
+      public function getExpiration()
+      {
+            return $this->expiration;
+      }
+
+      /**
+       * Set the value of expiration
+       */
+      public function setExpiration($expiration): self
+      {
+            $this->expiration = $expiration;
+
+            return $this;
+      }
   }
 
 
